26d3528b2cee4b77736ab7cb45f9bf0bdd5ec58f
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-08-04 13:43:34 +0500

    salom dunyo

---
+++ js.js
@@ -0,0 +1,101 @@
+// let div = document.querySelector(".moshina");
+
+// let countleft = 0;
+// let  countright = 0;
+// let  counttop = 0;
+// let  countbottom = 0;
+// let   tenpx = "10px";
+// 6
+
+// document.addEventListener("keydown",(e) =>{
+//     if(e.keyCode == 37){
+//         countleft++
+//         countright--
+//         div.style.left = countright*-10+"px"
+//     }
+//     if(e.keyCode == 38){
+//         countbottom--;
+//         counttop++
+
+//         div.style.top = counttop*-10+"px"
+//     }
+//     if(e.keyCode == 39){
+//         countright++
+//         countleft--;
+//         div.style.right = countleft* 10+"px"
+//     }
+//     if(e.keyCode == 40){
+//         countbottom++;
+//         counttop--
+
+
+//         div.style.bottom = countbottom* 10+"px"
+//     }
+
+// })
+
+
+
+
+
+// const person = {fname:"John", lname:"Doe", age:25};
+
+// let text = "";
+// for (let x in person) {
+//   text += person[x];
+// }
+
+// console.log(person);
+ 
+
+// // const cars = ["BMW", "Volvo", "Mini"];
+
+// // let text = "";
+// // for (let x in cars) {
+// //   text += x;
+// // }
+
+
+
+// // console.log(cars)
+
+// const timer = setTimeout(function () {
+//     alert("salom")
+
+// }, 2000)
+    
+// const time = setTimeout(function () {
+//     alert("yoqol saytdan yoqmissa")
+
+// }, 3000)
+// const clock = setTimeout(function () {
+
+
+//     alert("nma dissa kalta yevolasan hozir")
+// }, 4000)
+
+    
+// let data = {
+//     Id:12,
+//     Ism:"bekzodbek"
+// }
+
+
+
+// console.log(data.length)
+
+
+
+
+
+
+// console.log(data.length)
+
+// let dat ={id:12, ism: "nnv"}
+//  console.log(dat.id)
+
+
+
+
+// let p = ["ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses","ses"]
+// console.log(p)
